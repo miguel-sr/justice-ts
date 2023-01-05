a8b44ea79f11d1da857d679e5d973f0424eafe1c
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-04 22:27:49 -0300

    :sparkles: feat: Add 'Category' Controller

--- backend/src/controllers/category/get-categories/get-categories.ts
+++ backend/src/controllers/category/get-categories/get-categories.ts
@@ -4,14 +4,16 @@ import { IController, IHttpRequest, IHttpResponse } from "../../protocols";
 import { IGetCategoriesRepository } from "./protocols";
 
 export class GetCategoriesController implements IController {
-  constructor(private readonly getUsersRepository: IGetCategoriesRepository) {}
+  constructor(
+    private readonly getCategoriesRepository: IGetCategoriesRepository
+  ) {}
 
   async handle(
     httpRequest: IHttpRequest<unknown>
   ): Promise<IHttpResponse<Category[] | Category | string>> {
     try {
       const id = httpRequest?.params?.id;
-      const categories = await this.getUsersRepository.getCategories(id);
+      const categories = await this.getCategoriesRepository.getCategories(id);
       return ok<Category[] | Category>(categories);
     } catch (error) {
       return serverError();
