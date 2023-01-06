2adea8b614a0e2c6c6b7c39eb2c379238ba28a5b
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-06 11:02:18 -0300

    :sparkles: feat: Add 'Member' Repository

--- backend/src/repositories/member/create-member/mongo-create-member.ts
+++ backend/src/repositories/member/create-member/mongo-create-member.ts
@@ -6,7 +6,7 @@ import { MongoClient } from "../../../database/mongo";
 import { Member } from "../../../models/member";
 import { MongoMember } from "../../mongo-protocols";
 
-export class MongoCreateSocialActionRepository
+export class MongoMemberActionRepository
   implements ICreateMemberRepository
 {
   async createMember(params: ICreateMemberParams): Promise<Member> {
