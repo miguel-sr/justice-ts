8bc51e40116fb11ba655d0f5d56d9a56e15a625b
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-11 20:30:22 -0300

    :sparkles: feat: Add 'User' Service

---
+++ frontend/src/services/user.service.ts
@@ -0,0 +1,29 @@
+import API from "@/services/api.service";
+import alertService from "./alert.service";
+
+export default {
+  async login(body: JSON) {
+    try {
+      const response = await API().post("/login", body);
+      const { token } = response.data;
+
+      localStorage.setItem("userToken", token);
+
+      alertService.success("Login realizado com sucesso.");
+    } catch (err) {
+      alertService.error("Erro ao realizar login.");
+    }
+  },
+  async logout() {
+    try {
+      await API().post("/logout", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      });
+      alertService.success("Logout realizado com sucesso.");
+    } catch (err) {
+      alertService.error("Erro ao realizar logout.");
+    }
+  },
+};
