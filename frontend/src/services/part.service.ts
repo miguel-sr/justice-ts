ecb6638857145e56405e9b034a3631c3b8e314d9
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-11 20:30:16 -0300

    :sparkles: feat: Add 'Part' Service

---
+++ frontend/src/services/part.service.ts
@@ -0,0 +1,49 @@
+import API from "@/services/api.service";
+import alertService from "./alert.service";
+
+export default {
+  async get(id?: string) {
+    const response = await API().get("/parts/" + id, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+      },
+    });
+    return response.data;
+  },
+  async post(body: JSON) {
+    try {
+      await API().post("/parts", body, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      });
+      alertService.success("Peça criada com sucesso.");
+    } catch (err) {
+      alertService.error("Erro ao criar peça.");
+    }
+  },
+  async update(id: string, body: JSON) {
+    try {
+      await API().patch("/parts/" + id, body, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      });
+      alertService.success("Peça atualizada com sucesso.");
+    } catch (err) {
+      alertService.error("Erro ao atualizar peça.");
+    }
+  },
+  async delete(id: string) {
+    try {
+      await API().delete("/parts/" + id, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      });
+      alertService.success("Peça deletada com sucesso.");
+    } catch (err) {
+      alertService.error("Erro ao deletar peça.");
+    }
+  },
+};
