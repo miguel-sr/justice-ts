f7dfa026f9a49d1e088786b59995ba150fd51e29
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-11 20:30:08 -0300

    :sparkles: feat: Add 'Order' Service

---
+++ frontend/src/services/order.service.ts
@@ -0,0 +1,37 @@
+import API from "@/services/api.service";
+import alertService from "./alert.service";
+
+export default {
+  async get(id?: string) {
+    const response = await API().get("/orders/" + id, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+      },
+    });
+    return response.data;
+  },
+  async post(body: JSON) {
+    try {
+      await API().post("/orders", body, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      });
+      alertService.success("Pedido criado com sucesso.");
+    } catch (err) {
+      alertService.error("Erro ao criar pedido.");
+    }
+  },
+  async delete(id: string) {
+    try {
+      await API().delete("/orders/" + id, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      });
+      alertService.success("Pedido deletado com sucesso.");
+    } catch (err) {
+      alertService.error("Erro ao deletar pedido.");
+    }
+  },
+};
