a8a6b4099d3d882f4c632ab5fc7dcbb46da51466
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-11 14:41:07 -0300

    :truck:

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import auth from "../middlewares/auth";
+import getCache from "../services/cache.service";
+import jwtService from "../services/jwt.service";
 import { CreateUserController } from "../controllers/user/create-user/create-user";
 import { DeleteUserController } from "../controllers/user/delete-user/delete-user";
 import { GetUsersController } from "../controllers/user/get-users/get-users";
@@ -8,6 +10,9 @@ import { MongoCreateUserRepository } from "../repositories/user/create-user/mong
 import { MongoDeleteUserRepository } from "../repositories/user/delete-user/mongo-delete-user";
 import { MongoGetUsersRepository } from "../repositories/user/get-users/mongo-get-users";
 import { MongoUpdateUserRepository } from "../repositories/user/update-user/mongo-update-user";
+import { MongoLoginUserRepository } from "../repositories/user/login-user/mongo-login-user";
+import { LoginUserController } from "../controllers/user/login-user/login-user";
+
 const routes = Router();
 
 routes.get("/users/:id?", auth, async (req, res) => {
@@ -51,6 +56,34 @@ routes.delete("/users/:id", auth, async (req, res) => {
     params: req.params,
   });
   res.status(statusCode).send(body);
+});
+
+routes.post("/login", async (req, res) => {
+  const mongoLoginUserRepository = new MongoLoginUserRepository();
+  const loginUsersController = new LoginUserController(
+    mongoLoginUserRepository
+  );
+  const { body, statusCode } = await loginUsersController.handle({
+    body: req.body,
+  });
+  res.status(statusCode).send(body);
+});
+
+routes.post("/logout", auth, async (req, res) => {
+  const redisClient = await getCache();
+
+  if (!req.headers.authorization) {
+    return res.status(400).json("Missing authorization header.");
+  }
+
+  const token = req.headers.authorization.replace("Bearer ", "");
+  const decoded = jwtService.verify(token);
+
+  const token_key = `bl_${token}`;
+  redisClient.set(token_key, token);
+  redisClient.expireAt(token_key, decoded.exp);
+
+  return res.status(200).send("Token invalidated.");
 });
 
 export default routes;
