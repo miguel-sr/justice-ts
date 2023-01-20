3b5518e2796bb9c21f984f664cbcdbb2fbf261ba
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-20 14:08:00 -0300

    :sparkles: feat: Add loading screen

--- frontend/src/views/videos/script.ts
+++ frontend/src/views/videos/script.ts
@@ -1,17 +1,21 @@
 import { defineComponent } from "vue";
+import LoadingComponent from "../../components/Loading/LoadingComponent.vue";
 import NavbarComponent from "../../components/Navbar/NavbarComponent.vue";
 import FooterComponent from "../../components/FooterComponent.vue";
 import VideoService from "../../services/server/video.service";
+import alertService from "@/services/alert.service";
 
 export default defineComponent({
   name: "VideosPage",
   components: {
+    LoadingComponent,
     NavbarComponent,
     FooterComponent,
   },
   data() {
     return {
       videos: null,
+      videosIsLoaded: false,
     };
   },
   mounted() {
@@ -19,7 +23,13 @@ export default defineComponent({
   },
   methods: {
     async loadData() {
-      this.videos = await VideoService.get();
+      try {
+        this.videos = await VideoService.get();
+      } catch (error) {
+        alertService.error("Erro ao carregar patrocinadores.");
+      } finally {
+        this.videosIsLoaded = true;
+      }
     },
   },
 });
