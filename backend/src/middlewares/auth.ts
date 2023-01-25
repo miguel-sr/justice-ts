dbd329e02a2c3f4915c55759385b1faf347c58fb
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-01-25 17:04:44 -0300

    :recycle: refactor: Remove redis

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -1,19 +1,13 @@
 import { NextFunction, Request, Response } from "express";
-import getCache from "../services/cache.service";
 import jwtService from "../services/jwt.service";
 
 async function auth(req: Request, res: Response, next: NextFunction) {
   try {
-    const redisClient = await getCache();
     if (!req.headers.authorization) {
       return res.status(400).json("Missing authorization header.");
     }
 
     const token = req.headers.authorization.replace("Bearer ", "");
-    const inDenyList = await redisClient.get(`bl_${token}`);
-    if (inDenyList) {
-      return res.status(401).json("Authorization denied.");
-    }
 
     const decoded = jwtService.verify(token);
 
